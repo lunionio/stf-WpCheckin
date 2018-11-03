c992a4830de6330449cd7f462f99f3b8d72abbd3
Gilberto Junior <gilbertojunior533@gmail.com>
2018-11-03 18:08:22 -0300

    Adicionado Migration para criação da base.

---
+++ WpCheckIn.Infrastructure/Migrations/20181103210538_Initial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WpCheckIn.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CheckIns",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Descricao = table.Column<string>(type: "varchar(200)", nullable: true),
+                    DataCriacao = table.Column<DateTime>(nullable: false),
+                    DataEdicao = table.Column<DateTime>(nullable: false),
+                    UsuarioCriacao = table.Column<int>(nullable: false),
+                    UsuarioEdicao = table.Column<int>(nullable: false),
+                    Ativo = table.Column<bool>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    IdCliente = table.Column<int>(nullable: false),
+                    CodigoExterno = table.Column<int>(nullable: false),
+                    IdUsuario = table.Column<int>(nullable: false),
+                    Longitude = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Latitude = table.Column<string>(type: "varchar(100)", nullable: true),
+                    QrCodeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckIns", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheckIns");
+        }
+    }
+}
